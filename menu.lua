9848abbc82c9f167fb6fb240872ee4c8488ea7a8
Vorondur <vorondur@gmail.com>
2021-10-28 19:10:14 -0500

    Added the rest of the names and fixed typo

--- menu.lua
+++ menu.lua
@@ -1,5 +1,5 @@
--- Matt ___, Sam Martin, Patrick Berzins, ____, _____  
---10/26/21 - added composer to go imediately to menu
+-- Matt Goodwin, Sam Martin, Patrick Berzins, Jeremiah Maynard, Benny Stellhorn  
+--10/26/21 - added composer to go immediately to menu
 
 local composer = require("composer")
 local scene = composer.newScene()
