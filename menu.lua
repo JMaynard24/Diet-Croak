050c10080a20621b35bfd015d0de414006c0a575
Vorondur <vorondur@gmail.com>
2021-11-27 15:33:43 -0600

    Changing spaces for tabs and fixing some spacing issues

--- menu.lua
+++ menu.lua
@@ -9,125 +9,111 @@ display.setStatusBar(display.HiddenStatusBar)
 
 
 function scene:create( event )
- 
-  
-   -- Initialize the scene here.
-   -- Example: add display objects to "sceneGroup", add touch listeners, etc.
+	-- options for play button
+    local playButtonOptions = 
+    {
+        x = display.contentCenterX,
+        y = display.contentCenterY,
+        label = "Play",
+        labelColor = {default = {1,1,1}, over = {1,1,1}},
+        font = "Arial",
+        fontSize = 60,
+        shape = "Rectangle",
+        width = 360,
+        height = 100,
+        fillColor = {default = {0,1,0}, over = {0,0,1}},
+        onEvent = onPlayButton,
+    }
+
+    -- create a play button to begin the game and move to the game screen
+    local playButton = widget.newButton(playButtonOptions)
+    sceneGroup:insert(playButton)
+
+    -- event handler function for options button
+    local function onOptionsButton(event)
+        -- load the options_menu overlay scene
+		if event.phase == "began" then
+			composer.showOverlay("options_main", {effect="fade", time=500, isModal=true})
+		end
+    end
+
+    -- options for options button
+    local optionsButtonOptions =
+    {
+        x = display.contentCenterX,
+        y = display.contentCenterY + 150,
+        label = "Options",
+        labelColor = {default = {1,1,1}, over = {1,1,1}},
+        font = "Arial",
+        fontSize = 60,
+        shape = "Rectangle",
+        width = 360,
+        height = 100,
+        fillColor = {default = {0,0,1}, over = {0,1,0}},
+        onEvent = onOptionsButton,
+    }
+
+    -- create an options button to bring up the options menu
+    local optionsButton = widget.newButton(optionsButtonOptions)
+    sceneGroup:insert(optionsButton)
 end
  
 -- "scene:show()"
 function scene:show( event )
  
-   local sceneGroup = self.view
-   local phase = event.phase
+	local sceneGroup = self.view
+	local phase = event.phase
  
-   if ( phase == "will" ) then
+	if ( phase == "will" ) then
 		print("menu scene")
-      -- Initialize the scene here.
-      -- Example: add display objects to "sceneGroup", add touch listeners, etc.
-      -- Called when the scene is still off screen (but is about to come on screen).
-
-      local sceneGroup = self.view
-
-      -- create a label for the game title
-      local titleLabel = display.newText("Diet Croak", display.contentCenterX,display.contentCenterY - 350, "Arial", 80)
-      sceneGroup:insert(titleLabel)
-
-      -- event handler function for the play button
-      local function onPlayButton(event)
-
-         -- go to the game screen
-		 if event.phase == "began" then
-			composer.gotoScene("game")
-		 end
-
-      end
-
-      -- options for play button
-      local playButtonOptions = 
-      {
-         x = display.contentCenterX,
-         y = display.contentCenterY,
-         label = "Play",
-         labelColor = {default = {1,1,1}, over = {1,1,1}},
-         font = "Arial",
-         fontSize = 60,
-         shape = "Rectangle",
-         width = 360,
-         height = 100,
-         fillColor = {default = {0,1,0}, over = {0,0,1}},
-         onEvent = onPlayButton,
-      }
-
-      -- create a play button to begin the game and move to the game screen
-      local playButton = widget.newButton(playButtonOptions)
-      sceneGroup:insert(playButton)
-
-      -- event handler function for options button
-      local function onOptionsButton(event)
-         
-         -- load the options_menu overlay scene
-		 if event.phase == "began" then
-			composer.showOverlay("options_main", {effect="fade", time=500, isModal=true})
-		 end
-
-      end
-
-      -- options for options button
-      local optionsButtonOptions =
-      {
-         x = display.contentCenterX,
-         y = display.contentCenterY + 150,
-         label = "Options",
-         labelColor = {default = {1,1,1}, over = {1,1,1}},
-         font = "Arial",
-         fontSize = 60,
-         shape = "Rectangle",
-         width = 360,
-         height = 100,
-         fillColor = {default = {0,0,1}, over = {0,1,0}},
-         onEvent = onOptionsButton,
-      }
-
-      -- create an options button to bring up the options menu
-      local optionsButton = widget.newButton(optionsButtonOptions)
-      sceneGroup:insert(optionsButton)
-
       
 
+		local sceneGroup = self.view
 
-   elseif ( phase == "did" ) then
+		-- create a label for the game title
+		local titleLabel = display.newText("Diet Croak", display.contentCenterX,display.contentCenterY - 350, "Arial", 80)
+		sceneGroup:insert(titleLabel)
+
+		-- event handler function for the play button
+		local function onPlayButton(event)
+
+         -- go to the game screen
+		if event.phase == "began" then
+			composer.gotoScene("game")
+		end
+
+    elseif ( phase == "did" ) then
       -- Called when the scene is now on screen.
       -- Insert code here to make the scene come alive.
       -- Example: start timers, begin animation, play audio, etc.
-   end
+    end
 end
  
 -- "scene:hide()"
 function scene:hide( event )
  
-   local phase = event.phase
+	local phase = event.phase
    
 
-   if ( phase == "will" ) then
-      -- Called when the scene is on screen (but is about to go off screen).
-      -- Insert code here to "pause" the scene.
-      -- Example: stop timers, stop animation, stop audio, etc.
-   elseif ( phase == "did" ) then
-      local sceneGroup = self.view
-      -- Called immediately after scene goes off screen.
-   end
+	if ( phase == "will" ) then
+		-- Called when the scene is on screen (but is about to go off screen).
+		-- Insert code here to "pause" the scene.
+		-- Example: stop timers, stop animation, stop audio, etc.
+	elseif ( phase == "did" ) then
+		local sceneGroup = self.view
+		-- Called immediately after scene goes off screen.
+	end
 end
  
 -- "scene:destroy()"
 function scene:destroy( event )
  
-   local sceneGroup = self.view
+	local sceneGroup = self.view
  
  
-   -- Called prior to the removal of scene's view ("sceneGroup").
-   -- Insert code here to clean up the scene.
-   -- Example: remove display objects, save state, etc.
+	-- Called prior to the removal of scene's view ("sceneGroup").
+	-- Insert code here to clean up the scene.
+	-- Example: remove display objects, save state, etc.
 end
  
 ---------------------------------------------------------------------------------
